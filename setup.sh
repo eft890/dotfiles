c50b7f57367b666b285dfd44fbb3bf250c3800e4
Edd Tsao <edd.tsao@carinacare.com>
2016-10-12 11:02:08 -0700

    Add zshenv backup before ln

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ if [ -f ~/.zshrc ] || [ -L ~/.zshrc ] ; then
   echo '.zshrc already exists. Backing up as ~/.zshrc.bak.'
 fi
 ln -s $DIR/zshrc ~/.zshrc
+# Symbolic link zshenv
+if [ -f ~/.zshenv ] || [ -L ~/.zshenv ] ; then
+  mv ~/.zshenv ~/.zshenv.bak
+  echo '.zshenv already exists. Backing up as ~/.zshenv.bak.'
+fi
 ln -s $DIR/zshenv ~/.zshenv
 
 # Symbolic link nvim config
